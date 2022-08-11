bbc5296d6696f4610177d2422feaead6b2e631f6
SadeepaMedhan <sadeepamedhan823@gmail.com>
2022-08-11 12:22:23 +0530

    update

--- src/Client/ClientFormController.java
+++ src/Client/ClientFormController.java
@@ -1,11 +1,44 @@
 package Client;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
 
 public class ClientFormController {
+    public TextField txtClientMessage;
     Socket socket = null;
+    String message = "";
+
     public void initialize() throws IOException {
-        socket = new Socket("localhost",5000);
+        new Thread(()->{
+            try {
+                socket = new Socket("localhost",5000);
+
+            while (true){
+                if(!message.equals("exit")){
+                    InputStreamReader inputStreamReader = new InputStreamReader(socket.getInputStream());
+                    BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                    String record = bufferedReader.readLine();
+                    message = record;
+                    System.out.println(record);
+                }else{
+                    return;
+                }
+            }} catch (IOException e) {
+                e.printStackTrace();
+            }
+        }).start();
+
+    }
+
+    public void sendOnAction(ActionEvent actionEvent) throws IOException {
+        PrintWriter printWriter = new PrintWriter(socket.getOutputStream());
+        printWriter.println(txtClientMessage.getText());
+        printWriter.flush();
     }
 }
--- src/Server/ServerFormController.java
+++ src/Server/ServerFormController.java
@@ -1,19 +1,48 @@
 package Server;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class ServerFormController {
+    public TextField txtServerMessage;
+    String message = "";
+    Socket accept=null;
     public void initialize(){
         new Thread(() -> {
             try {
                 ServerSocket serverSocket = new ServerSocket(5000);
                 System.out.println("server start..");
-                Socket accept = serverSocket.accept();
+                accept = serverSocket.accept();
                 System.out.println("client connected!");
+
+                while (true){
+                    if(!message.equals("exit")){
+                        InputStreamReader inputStreamReader = new InputStreamReader(accept.getInputStream());
+                        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                        String record = bufferedReader.readLine();
+                        message = record;
+                        System.out.println(record);
+                    }else{
+                        return;
+                    }
+                }
+
             }catch (Exception e){
                 e.printStackTrace();
             }
         }).start();
+    }
+
+    public void sendOnAction(ActionEvent actionEvent) throws IOException {
+        PrintWriter printWriter = new PrintWriter(accept.getOutputStream());
+        printWriter.println(txtServerMessage.getText());
+        printWriter.flush();
     }
 }
